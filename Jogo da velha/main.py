f2048638838e86b9bf7a5e8d5d80422816b52169
kgazz <73496427+kgazz@users.noreply.github.com>
2022-12-13 00:43:03 -0300

    Correção de erros na logica

--- Jogo da velha/main.py
+++ Jogo da velha/main.py
@@ -1,9 +1,10 @@
 import random
-import os
+import os 
 
 lista = []
 matriz = []
 ganhador = False
+jogadas = 0
 vez = 0
 
 # Gerando uma matriz de elementos vazios para ser a 'velha'
@@ -28,68 +29,82 @@ def imprimir_velha():
 
 def winner():
     global ganhador
-    if matriz[0][0] == matriz[0][1] == matriz[0][2]:
-        if matriz[0][2] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[0][2] == 'O':
-            print('Você perdeu o jogo :( ')
+    global jogadas
+
+    # definindo as 3 possiveis vitorias na vertical 
+    if matriz[0][0] == matriz[0][1] == matriz[0][2] != " ":
         ganhador = True
-    elif matriz[1][0] == matriz[1][1] == matriz[1][2]:
-        if matriz[1][0] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[1][0] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[0][0] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")    
+    elif matriz[1][0] == matriz[1][1] == matriz[1][2] != " ":
         ganhador = True
-    elif matriz[2][0] == matriz[2][1] == matriz[2][2]:
-        if matriz[2][0] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[2][0] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[1][0] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+    elif matriz[2][0] == matriz[2][1] == matriz[2][2] != " ":
         ganhador = True
-    elif matriz[0][0] == matriz[1][0] == matriz[2][0]:
-        if matriz[0][0] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[0][0] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[2][0] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+
+    # definindo as 3 possiveis vitorias na horizontal 
+
+    elif matriz[0][0] == matriz[1][0] == matriz[2][0] != " ":
         ganhador = True
-    elif matriz[0][1] == matriz[1][1] == matriz[2][1]:
-        if matriz[0][1] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[0][1] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[0][0] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+    elif matriz[0][1] == matriz[1][1] == matriz[2][1] != " ":
         ganhador = True
-    elif matriz[0][2] == matriz[1][2] == matriz[2][2]:
-        if matriz[0][2] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[0][2] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[0][1] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+    elif matriz[0][2] == matriz[1][2] == matriz[2][2] != " ":
         ganhador = True
-    elif matriz[0][0] == matriz[1][1] == matriz[2][2]:
-        if matriz[1][1] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[1][1] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[0][2] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+
+    # definindo as 2 possiveis vitorias na diagonal 
+
+    elif matriz[0][0] == matriz[1][1] == matriz[2][2] != " ":
         ganhador = True
-    elif matriz[0][2] == matriz[1][1] == matriz[2][0]:
-        if matriz[2][0] == 'X':
-            print('Parabéns, você ganhou o jogo!!')
-        elif matriz[2][0] == 'O':
-            print('Você perdeu o jogo :( ')
+        if matriz[0][0] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+    elif matriz[0][2] == matriz[1][1] == matriz[2][0] != " ":
         ganhador = True
+        if matriz[0][2] == "X":
+            print("Parabéns, você ganhou!! ")
+        else:
+            print("Não foi dessa vez, você perdeu :( ")
+
+    #possivel condição de dar velha
+
+    elif jogadas == 9 and ganhador == False:
+        print("Deu velha!")
 
 
 
 def jogo():
-    winner()
-    jogadas = 0
+    global jogadas
     imprimir_velha()
     i = int(input("Determine a linha da sua jogada: "))
     j = int(input("Determine a coluna da sua jogada: "))
     matriz[i][j] = "X"
     vez = 1
     os.system("cls")
-    
+
     while jogadas < 9 and ganhador == False:
+        winner()
         if vez == 1 and  ganhador == False:
             l = random.randrange(0, 3)
             c = random.randrange(0, 3)
@@ -107,9 +122,12 @@ def jogo():
                 vez = 1 
                 jogadas += 1
                 imprimir_velha()
-            #print(matriz[0][0] + matriz[0][1] + matriz[0][2])
-            os.system("cls")      
-        
+            elif matriz[i][j] == "X" or matriz[i][j] == "O":
+                print("Houve um erro. \n Determine sua jogada novamente. ")
+                imprimir_velha()
+            
+            os.system("cls")
+
 
 
 def main():
@@ -118,7 +136,7 @@ def main():
         print('=-'*15)
         print('  ' + 'Bem vindo ao jogo da velha')
         print('-='*15 + "\n")
-        print("Jogo da velha tradicional, para escolher \na posição desejada o jogador vai selecionar \na linha e coluna desejada para jogar \n \nPor exemplo '0 0' ou '0 1'. ")
+        print("Jogo da velha tradicional, para escolher \na posição desejada o jogador vai selecionar \na linha e coluna desejada para jogar")
         jogo()
 
     elif iniciar == "n" or iniciar == "N":
